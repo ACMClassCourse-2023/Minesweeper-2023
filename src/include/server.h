7d4146b3f440be77264819285c54c5c85273e190
Conless <conlesspan@outlook.com>
2023-09-17 23:12:05 +0800

    feat(src/server): add comments for server's code.

--- src/include/server.h
+++ src/include/server.h
@@ -3,14 +3,102 @@
 
 #include <iostream>
 
-void ReadMap();
-void ReadMapFromString(const std::string &input);
+/**
+ * @brief The definition of function ReadMap()
+ *
+ * @details This function is designed to read the initial map from stdin. For example, if there is a 3 * 3 map in which
+ * mines are located at (0, 1) and (1, 2) (0-based), the stdin would be
+ *     3 3
+ *     .@.
+ *     ...
+ *     ..@
+ * where @ stands for a mine block and . stands for a normal block. After executing this function, your game map would
+ * be initialized, with all the blocks unvisited.
+ *
+ * @param is_initializing (for A-class only) This parameter would always be true and unused in basic test cases. That
+ * means you don't need to read the contents after here. If you're trying advanced mission, this value would be false
+ * when you (a.k.a. the client) call it. Since the client (or player) can only get the limited information of the game
+ * map, so if there is a 3 * 3 map as above and only the block (2, 0) has been visited, the stdin would be
+ *     ...
+       12.
+       01.
+ * Therefore, we recommend you to use this structure to read the map if you're trying the advanced mission
+ *     void ReadMap(bool is_initializing) {
+ *       if (!is_initializing) {
+ *         // read the whole information about the game map as server
+ *       } else {
+ *         // read the limited information about the game map as client
+ *       }
+ *     }
+ */
+void ReadMap(bool is_initializing = true);
 
-auto VisitBlock(unsigned pos_x, unsigned pos_y) -> int;
+/**
+ * @brief The definition of function VisitBlock(int, int)
+ *
+ * @details This function is designed to visit a block in the game map. We take the 3 * 3 game map above as an example.
+ * At the beginning, if you call VisitBlock(0, 0), the return value would be 0 (game continues), and the game map would
+ * be
+ *     1..
+ *     ...
+ *     ...
+ * If you call VisitBlock(0, 1) after that, the return value would be -1 (game ends and the players loses) , and the
+ * game map would be
+ *     1@.
+ *     ...
+ *     ...
+ * If you call VisitBlock(0, 2), VisitBlock(2, 0), VisitBlock(1, 2) instead, the return value of the last operation
+ * would be 1 (game ends and the player wins), and the game map would be
+ *    1.1
+ *    122
+ *    01.
+ *
+ * @param pos_x The x-coordinate of the block to be visited.
+ * @param pos_y The y-coordinate of the block to be visited.
+ * @return int
+ *    0  if the game continues after visit that block, or that block has already been visited before.
+ *    1  if the game ends and the player wins.
+ *    -1 if the game ends and the player loses.
+ */
+auto VisitBlock(unsigned int pos_x, unsigned int pos_y) -> int;
 
+/**
+ * @brief The definition of function PrintMap()
+ *
+ * @details This function is designed to print the game map to stdout. We take the 3 * 3 game map above as an example.
+ * At the beginning, if you call PrintMap(), the stdout would be
+ *    ...
+ *    ...
+ *    ...
+ * If you call VisitBlock(2, 0) and PrintMap() after that, the stdout would be
+ *    ...
+ *    12.
+ *    01.
+ * If you call VisitBlock(0, 1) and PrintMap() after that, the stdout would be
+ *    .@.
+ *    12.
+ *    01.
+ */
 void PrintMap();
-void PrintMapToString(std::string &output);
 
+/**
+ * @brief The definition of function ExitGame()
+ *
+ * @details This function is designed to exit the game. It outputs two integers, visit_count and step_count,
+ * representing the number of blocks visited and the number of steps taken respectively.
+ */
 void ExitGame();
+
+/**
+ * @brief The definition of function Execute(unsigned, unsigned), ReadMapFromString(string) and PrintMapToString(string)
+ * (for A-class only)
+ *
+ * @details You can use this function to read the game map when playing the client's (or player) role. If you don't
+ * understand the use of these functions, please ask the TAs for help. Notice that you shouldn't modify the
+ * implementation of them. If you must, please ask the TA in advance.
+ */
+auto Execute(unsigned int pos_x, unsigned int pos_y) -> int;
+void ReadMapFromString(const std::string &input);
+void PrintMapToString(std::string &output);
 
 #endif
--- src/server.cpp
+++ src/server.cpp
@@ -1,16 +1,75 @@
+/* Copyright (c) 2023 ACM Class Course
+ * This software is distributed under the MIT License.
+ *
+ * This file (server.cpp) is an implementation for server.h. When running the basic test cases, the main file (main.cpp)
+ * would call the functions in this file to perform the server's job in default structure.
+ */
+
 #include "server.h"
 
 #include <iostream>
 #include <sstream>
 #include <string>
 
+/*
+ * You may need to define some global variables for the information of the game map here.
+ * Although we don't encourage to uss global variables in real cpp projects, you may have to use them because the use of
+ * class is not taught yet. However, if you are member of A-class or have learnt the use of cpp class, member functions,
+ * etc., you're free to modify this structure.
+ */
+
+/**
+ * @brief The implementation of function ReadMap
+ * @details Implement me!
+ * @param is_initializing leave it unused unless you're
+ */
+void ReadMap(bool is_initializing) {
+  // TODO (student)
+}
+
+auto VisitBlock(unsigned int pos_x, unsigned int pos_y) -> int {
+  return 0;
+}
+
+/**
+ * @brief The implementation of function PrintMap
+ * @details Implement me!
+ */
+void PrintMap() {
+  // TODO (student)
+}
+
+/**
+ * @brief The implementation of function Execute
+ * @details Use it only when trying advanced mission. Do NOT modify it before discussing with TA.
+ * @param pos_x 
+ * @param pos_y 
+ */
+auto Execute(unsigned int pos_x, unsigned int pos_y) -> int {
+  std::string str;
+  int result = VisitBlock(pos_x, pos_y);
+  PrintMapToString(str);
+  ReadMapFromString(str);
+  return result;
+}
+
+/**
+ * @brief The implementation of function ReadMapFromString
+ * @details Use it only when trying advanced mission. Do NOT modify it before discussing with TA.
+ * @param input 
+ */
 void ReadMapFromString(const std::string &input) {
   std::istringstream iss(input);
   std::streambuf *oldInputBuffer = std::cin.rdbuf();
-  ReadMap();
+  ReadMap(false);
   std::cin.rdbuf(oldInputBuffer);
 }
 
+/**
+ * @brief The implementation of function PrintMapToString
+ * @details Use it only when trying advanced mission. Do NOT modify it before discussing with TA.
+ * @param output 
+ */
 void PrintMapToString(std::string &output) {
   std::ostringstream oss;
   std::streambuf *oldOutputBuffer = std::cout.rdbuf();
